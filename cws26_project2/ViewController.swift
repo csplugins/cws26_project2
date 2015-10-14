14b26a822877ddcc291da0609aaa8f21156664b4
Cody Skala <cws26@zips.uarkon.edu>
2015-10-14 15:55:13 -0400

    Added a nice feature to edit the default plist if you so choose. Else the defualt loads

--- cws26_project2/ViewController.swift
+++ cws26_project2/ViewController.swift
@@ -24,46 +24,104 @@ class ViewController: UIViewController , UITextFieldDelegate{
         animal3.delegate = self
         animal4.delegate = self
         animal5.delegate = self
-        guard let path = NSBundle.mainBundle().pathForResource("AnimalList", ofType: "plist") else {
-            print("Invalid path for plist")
-            return
-        }
         
         // load the dictionary
-        var data = NSDictionary(contentsOfFile: path) as? Dictionary<String, String>
-        // string from the dictionary
-        var s = data?["animal1"]
-        animal1.text = s
-        s = data?["animal2"]
-        animal2.text = s
-        s = data?["animal3"]
-        animal3.text = s
-        s = data?["animal4"]
-        animal4.text = s
-        s = data?["animal5"]
-        animal5.text = s
+        loadData()
     }
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        /*guard let path = NSBundle.mainBundle().pathForResource("AnimalList", ofType: "plist") else {
-            print("Invalid path for plist")
-            return false
+    
+    func loadData() {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as NSArray
+        let documentsDirectory = paths[0] as! String
+        let path = documentsDirectory+"/rdata.plist"
+        let fileManager = NSFileManager.defaultManager()
+        //check if file exists
+        if(!fileManager.fileExistsAtPath(path)) {
+            // If it doesn't, copy it from the default file in the Bundle
+            if let bundlePath = NSBundle.mainBundle().pathForResource("AnimalList", ofType: "plist") {
+                let animalDictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+                print("Bundle GameData.plist file is --> \(animalDictionary?.description)")
+                do{
+                    try fileManager.copyItemAtPath(bundlePath, toPath: path)
+                }
+                catch{
+                    print("Could not copy the bundled path")
+                }
+                print("Copied successfully")
+            } else {
+                print("AnimalList.plist not found. Please, make sure it is part of the bundle.")
+            }
+        } else {
+            print("rdata.plist already exits at path.")
+            // use this to delete file from documents directory
+            /*do{
+                try fileManager.removeItemAtPath(path)
+            }
+            catch{
+                print("Removed")
+            }*/
         }
-        
-        let dict = NSMutableDictionary(contentsOfFile: path)
-        dict!.setValue("Test", forKey: "animal1")
-        if dict!.writeToFile(path, atomically: true){
-            print("plist_write")
-        }else{
-            print("plist_write_error")
-        }*/
-        
-        return true
+        let rdataDictionary = NSMutableDictionary(contentsOfFile: path)
+        print("Loaded rdata.plist file is --> \(rdataDictionary?.description)")
+        let myDict = NSDictionary(contentsOfFile: path)
+        if let dict = myDict {
+            //loading values
+            var s = dict.valueForKey("animal1") as! String
+            animal1.text = s
+            s = dict.valueForKey("animal2") as! String
+            animal2.text = s
+            s = dict.valueForKey("animal3") as! String
+            animal3.text = s
+            s = dict.valueForKey("animal4") as! String
+            animal4.text = s
+            s = dict.valueForKey("animal5") as! String
+            animal5.text = s
+        } else {
+            print("WARNING: Couldn't create dictionary from rdata.plist! Default values will be used!")
+            guard let path = NSBundle.mainBundle().pathForResource("AnimalList", ofType: "plist") else {
+                print("Invalid path for plist")
+                return
+            }
+            var data = NSDictionary(contentsOfFile: path) as? Dictionary<String, String>
+            var s = data?["animal1"]
+            animal1.text = s
+            s = data?["animal2"]
+            animal2.text = s
+            s = data?["animal3"]
+            animal3.text = s
+            s = data?["animal4"]
+            animal4.text = s
+            s = data?["animal5"]
+            animal5.text = s
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        let documentDir = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        let url = NSURL(string: documentDir)
+        guard let docURL = url?.URLByAppendingPathComponent("rdata.plist")else{ return false }
+        if let docURLPath = docURL.path{
+            var rdata = NSDictionary(contentsOfFile: docURLPath) as? Dictionary <String, String>
+            var temp = animal1.text
+            rdata?["animal1"] = temp
+            temp = animal2.text
+            rdata?["animal2"] = temp
+            temp = animal3.text
+            rdata?["animal3"] = temp
+            temp = animal4.text
+            rdata?["animal4"] = temp
+            temp = animal5.text
+            rdata?["animal5"] = temp
+            if (rdata! as NSDictionary).writeToFile(docURLPath, atomically: true){
+                print("Write success")
+            }
+        }
+        return true
     }
 
 }
